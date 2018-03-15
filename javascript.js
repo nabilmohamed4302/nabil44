43fef2aae8bba028447f6534c0753f84cc3a60dd
nabilmohamed4302 <37380888+nabilmohamed4302@users.noreply.github.com>
2018-03-15 03:26:42 +0200

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,33 @@
+    <!-- Start Script Images Website -->
+    <script src='https://rawgit.com/100px/js/master/read-more.js'/>
+    <script type='text/javascript'>var thumbnail_mode = &quot;no-float&quot; ;
+    summary_noimg = 300; /* حجم النص بدون صورة */
+    summary_img = 220; /* حجم النص مرفوق بصورة*/
+    img_thumb_height = 170; /* طول الصورة في الملخص */
+    img_thumb_width = 270; /* عرض الصورة في الملخص */
+    </script>
+    <!-- End Script Images Website -->
+     <!-- Start ToolTip -->
+    <script>
+      $(function () {
+        $(&#39;[data-toggle=&quot;tooltip&quot;]&#39;).tooltip()
+      })
+    </script>
+    <!-- End ToolTip -->
+    <!-- Start Tap -->
+    <script>
+    $(&#39;#myTabs a&#39;).click(function (e) {
+      e.preventDefault()
+      $(this).tab(&#39;show&#39;)
+    })
+    </script>
+    <!-- End Tap -->
+    <!-- Start Top -->
+  <a href="javascript:void(0);" id="rocketmeluncur" class="showrocket" ><i></i></a>
+
+  <script type='text/javascript'>
+    //<![CDATA[
+    jQuery(window).scroll(function(){jQuery(window).scrollTop()<50?jQuery("#rocketmeluncur").slideUp(500):jQuery("#rocketmeluncur").slideDown(500);var e=jQuery("#ft")[0]?jQuery("#ft")[0]:jQuery(document.body)[0],t=$("rocketmeluncur"),n=(parseInt(document.documentElement.clientHeight),parseInt(document.body.getBoundingClientRect().top),parseInt(e.clientWidth)),r=t.clientWidth;if(1e3>n){var l=parseInt(fetchOffset(e).left);l=r>l?2*l-r:l,t.style.left=n+l+"px"}else t.style.left="auto",t.style.right="10px"}),jQuery("#rocketmeluncur").click(function(){jQuery("html, body").animate({scrollTop:"0px",display:"none"},{duration:600,easing:"linear"});var e=this;this.className+=" launchrocket",setTimeout(function(){e.className="showrocket"},800)});
+    //]]>
+  </script>
+ <!-- End Top -->
